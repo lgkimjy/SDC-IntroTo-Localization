0dbd3d32041261b6ee1a24d5a99c9ee5599ab683
lgkimjy <lgkimjy@naver.com>
2021-03-19 19:22:21 +0900

    basic concept of markov

---
+++ markov_theory.py
@@ -0,0 +1,21 @@
+"""
+ *  probabilistic_robotics.py
+ *   Created on : 2021. 3. 18.
+ *       Author : Junyoung Kim
+"""
+
+import numpy as np
+
+p = [0.2, 0.2, 0.2, 0.2, 0.2]
+world = ['green', 'red', 'red', 'green', 'green']
+senses = ["miss", "hit", "hit", "miss", "miss"]
+Z = 'red'
+measurements = ['red', 'green']
+
+
+def markov(p, u, z, x):
+
+	return p
+
+
+print(markov(p, u, z, x))
