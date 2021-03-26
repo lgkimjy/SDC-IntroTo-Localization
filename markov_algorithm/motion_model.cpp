cc83fd4fbf682786cd8f1814001eb835e63dd676
lgkimjy <lgkimjy@naver.com>
2021-03-26 23:05:15 +0900

    motion model framework

---
+++ markov_algorithm/motion_model.cpp
@@ -0,0 +1,28 @@
+/*
+ *  motion_model.cpp
+ *   Created on : 2021. 3. 26.
+ *       Author : Junyoung Kim
+ */
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+float motion_model(float pseudo_position, float movement, vector<float> priors, int map_size, int control_stdev);
+
+int main(){
+
+    int x, u, z, m;
+    int std_dev;
+    vector<float> priors;
+
+    return 0;
+}
+
+float motion_model(float pseudo_position, float movement, vector<float> priors, int map_size, int control_stdev){
+
+    float prob = 0;
+
+    return prob;
+}
